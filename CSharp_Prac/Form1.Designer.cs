cb76eced9fb0ef07e7ff807d79ada797662bd1f5
JeongGyuEop <gueeop0219@naver.com>
2025-08-22 13:13:42 +0900

    C# Solution & Thread Project Init

---
+++ CSharp_Prac/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace CSharp_Prac
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Btn_JulCha = new System.Windows.Forms.Button();
+            this.Btn_Thread = new System.Windows.Forms.Button();
+            this.Btn_JulCha2 = new System.Windows.Forms.Button();
+            this.Btn_Thread2 = new System.Windows.Forms.Button();
+            this.Btn_Thread2_1 = new System.Windows.Forms.Button();
+            this.Btn_Thread2_2 = new System.Windows.Forms.Button();
+            this.Btn_Foreground = new System.Windows.Forms.Button();
+            this.Btn_Background = new System.Windows.Forms.Button();
+            this.Btn_Start = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Btn_FileOpen = new System.Windows.Forms.Button();
+            this.Btn_Thread_Start = new System.Windows.Forms.Button();
+            this.Btn_JoinThread_Start = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Btn_JulCha
+            // 
+            this.Btn_JulCha.Font = new System.Drawing.Font("굴림", 20F);
+            this.Btn_JulCha.Location = new System.Drawing.Point(12, 157);
+            this.Btn_JulCha.Name = "Btn_JulCha";
+            this.Btn_JulCha.Size = new System.Drawing.Size(137, 76);
+            this.Btn_JulCha.TabIndex = 0;
+            this.Btn_JulCha.Text = "절차";
+            this.Btn_JulCha.UseVisualStyleBackColor = true;
+            this.Btn_JulCha.Click += new System.EventHandler(this.Btn_JulCha_Click);
+            // 
+            // Btn_Thread
+            // 
+            this.Btn_Thread.Font = new System.Drawing.Font("굴림", 20F);
+            this.Btn_Thread.Location = new System.Drawing.Point(155, 157);
+            this.Btn_Thread.Name = "Btn_Thread";
+            this.Btn_Thread.Size = new System.Drawing.Size(137, 76);
+            this.Btn_Thread.TabIndex = 1;
+            this.Btn_Thread.Text = "뜨레드";
+            this.Btn_Thread.UseVisualStyleBackColor = true;
+            this.Btn_Thread.Click += new System.EventHandler(this.Btn_Thread_Click);
+            // 
+            // Btn_JulCha2
+            // 
+            this.Btn_JulCha2.Font = new System.Drawing.Font("굴림", 20F);
+            this.Btn_JulCha2.Location = new System.Drawing.Point(12, 239);
+            this.Btn_JulCha2.Name = "Btn_JulCha2";
+            this.Btn_JulCha2.Size = new System.Drawing.Size(137, 76);
+            this.Btn_JulCha2.TabIndex = 2;
+            this.Btn_JulCha2.Text = "절차2";
+            this.Btn_JulCha2.UseVisualStyleBackColor = true;
+            this.Btn_JulCha2.Click += new System.EventHandler(this.Btn_JulCha2_Click);
+            // 
+            // Btn_Thread2
+            // 
+            this.Btn_Thread2.Font = new System.Drawing.Font("굴림", 20F);
+            this.Btn_Thread2.Location = new System.Drawing.Point(155, 239);
+            this.Btn_Thread2.Name = "Btn_Thread2";
+            this.Btn_Thread2.Size = new System.Drawing.Size(137, 76);
+            this.Btn_Thread2.TabIndex = 3;
+            this.Btn_Thread2.Text = "뜨레드2";
+            this.Btn_Thread2.UseVisualStyleBackColor = true;
+            this.Btn_Thread2.Click += new System.EventHandler(this.Btn_Thread2_Click);
+            // 
+            // Btn_Thread2_1
+            // 
+            this.Btn_Thread2_1.Font = new System.Drawing.Font("굴림", 20F);
+            this.Btn_Thread2_1.Location = new System.Drawing.Point(298, 239);
+            this.Btn_Thread2_1.Name = "Btn_Thread2_1";
+            this.Btn_Thread2_1.Size = new System.Drawing.Size(137, 76);
+            this.Btn_Thread2_1.TabIndex = 4;
+            this.Btn_Thread2_1.Text = "뜨레드2-1";
+            this.Btn_Thread2_1.UseVisualStyleBackColor = true;
+            this.Btn_Thread2_1.Click += new System.EventHandler(this.Btn_Thread2_1_Click);
+            // 
+            // Btn_Thread2_2
+            // 
+            this.Btn_Thread2_2.Font = new System.Drawing.Font("굴림", 20F);
+            this.Btn_Thread2_2.Location = new System.Drawing.Point(441, 239);
+            this.Btn_Thread2_2.Name = "Btn_Thread2_2";
+            this.Btn_Thread2_2.Size = new System.Drawing.Size(137, 76);
+            this.Btn_Thread2_2.TabIndex = 5;
+            this.Btn_Thread2_2.Text = "뜨레드2-2";
+            this.Btn_Thread2_2.UseVisualStyleBackColor = true;
+            this.Btn_Thread2_2.Click += new System.EventHandler(this.Btn_Thread2_2_Click);
+            // 
+            // Btn_Foreground
+            // 
+            this.Btn_Foreground.Font = new System.Drawing.Font("굴림", 20F);
+            this.Btn_Foreground.Location = new System.Drawing.Point(12, 361);
+            this.Btn_Foreground.Name = "Btn_Foreground";
+            this.Btn_Foreground.Size = new System.Drawing.Size(159, 76);
+            this.Btn_Foreground.TabIndex = 6;
+            this.Btn_Foreground.Text = "포그라운드";
+            this.Btn_Foreground.UseVisualStyleBackColor = true;
+            // 
+            // Btn_Background
+            // 
+            this.Btn_Background.Font = new System.Drawing.Font("굴림", 20F);
+            this.Btn_Background.Location = new System.Drawing.Point(177, 361);
+            this.Btn_Background.Name = "Btn_Background";
+            this.Btn_Background.Size = new System.Drawing.Size(159, 76);
+            this.Btn_Background.TabIndex = 7;
+            this.Btn_Background.Text = "백그라운드";
+            this.Btn_Background.UseVisualStyleBackColor = true;
+            // 
+            // Btn_Start
+            // 
+            this.Btn_Start.Font = new System.Drawing.Font("굴림", 12F);
+            this.Btn_Start.Location = new System.Drawing.Point(12, 12);
+            this.Btn_Start.Name = "Btn_Start";
+            this.Btn_Start.Size = new System.Drawing.Size(97, 32);
+            this.Btn_Start.TabIndex = 8;
+            this.Btn_Start.Text = "작업 시작";
+            this.Btn_Start.UseVisualStyleBackColor = true;
+            this.Btn_Start.Click += new System.EventHandler(this.Btn_Start_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 15F);
+            this.label1.Location = new System.Drawing.Point(18, 95);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 20);
+            this.label1.TabIndex = 9;
+            // 
+            // Btn_FileOpen
+            // 
+            this.Btn_FileOpen.Font = new System.Drawing.Font("굴림", 12F);
+            this.Btn_FileOpen.Location = new System.Drawing.Point(12, 50);
+            this.Btn_FileOpen.Name = "Btn_FileOpen";
+            this.Btn_FileOpen.Size = new System.Drawing.Size(97, 32);
+            this.Btn_FileOpen.TabIndex = 10;
+            this.Btn_FileOpen.Text = "파일 오픈";
+            this.Btn_FileOpen.UseVisualStyleBackColor = true;
+            this.Btn_FileOpen.Click += new System.EventHandler(this.Btn_FileOpen_Click);
+            // 
+            // Btn_Thread_Start
+            // 
+            this.Btn_Thread_Start.Font = new System.Drawing.Font("굴림", 12F);
+            this.Btn_Thread_Start.Location = new System.Drawing.Point(115, 12);
+            this.Btn_Thread_Start.Name = "Btn_Thread_Start";
+            this.Btn_Thread_Start.Size = new System.Drawing.Size(144, 32);
+            this.Btn_Thread_Start.TabIndex = 11;
+            this.Btn_Thread_Start.Text = "뜨레드 작업 시작";
+            this.Btn_Thread_Start.UseVisualStyleBackColor = true;
+            this.Btn_Thread_Start.Click += new System.EventHandler(this.Btn_Thread_Start_Click);
+            // 
+            // Btn_JoinThread_Start
+            // 
+            this.Btn_JoinThread_Start.Font = new System.Drawing.Font("굴림", 12F);
+            this.Btn_JoinThread_Start.Location = new System.Drawing.Point(265, 12);
+            this.Btn_JoinThread_Start.Name = "Btn_JoinThread_Start";
+            this.Btn_JoinThread_Start.Size = new System.Drawing.Size(170, 32);
+            this.Btn_JoinThread_Start.TabIndex = 12;
+            this.Btn_JoinThread_Start.Text = "Join 뜨레드 작업 시작";
+            this.Btn_JoinThread_Start.UseVisualStyleBackColor = true;
+            this.Btn_JoinThread_Start.Click += new System.EventHandler(this.Btn_JoinThread_Start_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Btn_JoinThread_Start);
+            this.Controls.Add(this.Btn_Thread_Start);
+            this.Controls.Add(this.Btn_FileOpen);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Btn_Start);
+            this.Controls.Add(this.Btn_Background);
+            this.Controls.Add(this.Btn_Foreground);
+            this.Controls.Add(this.Btn_Thread2_2);
+            this.Controls.Add(this.Btn_Thread2_1);
+            this.Controls.Add(this.Btn_Thread2);
+            this.Controls.Add(this.Btn_JulCha2);
+            this.Controls.Add(this.Btn_Thread);
+            this.Controls.Add(this.Btn_JulCha);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Btn_JulCha;
+        private System.Windows.Forms.Button Btn_Thread;
+        private System.Windows.Forms.Button Btn_JulCha2;
+        private System.Windows.Forms.Button Btn_Thread2;
+        private System.Windows.Forms.Button Btn_Thread2_1;
+        private System.Windows.Forms.Button Btn_Thread2_2;
+        private System.Windows.Forms.Button Btn_Foreground;
+        private System.Windows.Forms.Button Btn_Background;
+        private System.Windows.Forms.Button Btn_Start;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Btn_FileOpen;
+        private System.Windows.Forms.Button Btn_Thread_Start;
+        private System.Windows.Forms.Button Btn_JoinThread_Start;
+    }
+}
+
---
+++ CSharp_Prac/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace CSharp_Prac
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Btn_Start_Click(object sender, EventArgs e)
+        {
+            label1.Text = "작업 시작됨...";
+            /*
+                - 라벨이 바뀌기 전에 UI 멈춰서 표시 X
+                    라벨 변경이 실행된 직후에 바로 UI가 바뀌는게 아니라 UI 그리기 작업은 나중에 처리될 큐(Queue)에 등록됨.
+                    그래서 메인 스레드가 그리기 전에 멈추기 때문에 화면에 표시되지 않고, 코드 실행이 끝나고 나서야 한꺼번에 UI 갱신
+
+                - UI 자체가 응답이 없음
+                    Btn_Start는 메인 스레드에서 실행되는데, 오래걸리는 작업이 있으면 메인 스레드는 다른 작업을 못함.
+                    즉, 화면을 다시 그리는 일(UI 처리)도 못해서 응답 없음 상태가 됨.
+             */
+
+            // 오래 걸리는 작업 (메인 스레드에서 실행됨)
+            for (int i = 0; i < 5; i++)
+            {
+                Thread.Sleep(1000); // 1초씩 멈춤
+                Console.WriteLine($"진행 중 ... {i + 1}초");
+            }
+
+            label1.Text = "작업 완료!";
+        }
+        private void Btn_FileOpen_Click(object sender, EventArgs e)
+        {
+            label1.Text = "파일 읽는 중...";
+            string content = File.ReadAllText("C:\\큰파일.txt");  // 오래 걸리는 작업
+            label1.Text = "파일 읽기 완료!";
+        }
+
+        private void Btn_Thread_Start_Click(object sender, EventArgs e)
+        {
+            label1.Text = "작업 시작됨 ...";
+            Thread thread = new Thread(BackgroundWork);
+            thread.Start();
+        }
+        private void BackgroundWork()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine($"[스레드] {i + 1}초 경과");
+            }
+
+            /*
+                label1.Text = "작업 완료!";  
+                
+                오류 발생
+                lable1은 메인(UI) 스레드가 만든 컨트롤
+                다른 스레드에서는 label1을 직접 건드릴 수 없음
+                UI 작업은 메인스레드만 할 수 있도록 제한되어 있음
+            */
+
+            // Invoke: 메인 스레드에서 동작하도록 해주는 기능
+            this.Invoke(new MethodInvoker(UpdateLabel));
+            // InvokeRequired: Invoke가 필요한 상황인지 체크(현재 실행중인 스레드가 메인인지 아닌지)
+        }
+        private void UpdateLabel()
+        {
+            label1.Text = "작업 완료!";
+        }
+
+        private void Btn_JoinThread_Start_Click(object sender, EventArgs e)
+        {
+            label1.Text = "작업 시작됨 ...";
+
+            Thread thread = new Thread(LongTask);
+            thread.Start();
+
+            // 스레드가 끝날 때까지 기다리기
+            thread.Join(); // 메인 스레드를 여기서 멈춤!!
+
+            label1.Text = "작업 완료!";
+        }
+        private void LongTask()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine($"[LongTask] {i + 1}초 경과");
+            }
+        }
+
+        // ============================================================================================
+        // ============================================================================================
+        private void Btn_JulCha_Click(object sender, EventArgs e)
+        {
+            Run(0);
+            Run(1);
+        } // ========== 절차
+        private void Btn_Thread_Click(object sender, EventArgs e)
+        {
+            Thread thread0 = new Thread(new ParameterizedThreadStart(Run));
+            thread0.Start(0);
+            Thread.Sleep(100);
+            thread0.Abort(); // 함수의 종료를 보장하지 않고 어느 시점이던지 상관 없이 도중에 강제로 중단(예외 발생: 'System.Threading.ThreadAbortException')
+                             // 스레드 강제 종료가 시스템에 심각한 영향이 없다는 보장이 있을 때만 사용하는 것이 좋다.
+
+            Thread thread1 = new Thread(new ParameterizedThreadStart(Run));
+            thread1.Start(1); 
+            Thread.Sleep(100);
+            thread1.Join(); // 함수의 종료를 보장하며 스레드가 동작시키는 중인 함수의 끝에 도달하기를 기다린 다음 스레드 종료
+        } // ========== 스레드
+        void Run(object obj)
+        {
+            try
+            {
+                Console.WriteLine(string.Format("Run {0} Start", obj));
+
+                for (int i=0; i<100; i++)
+                {
+                    Console.WriteLine(string.Format("Run {0} :: {1}", obj, i));
+                    Thread.Sleep(10);
+                }
+
+                Console.WriteLine(string.Format("Run {0} End", obj));
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        } // 호출 함수
+
+        private void Btn_JulCha2_Click(object sender, EventArgs e)
+        {
+            Run2();
+        }
+        private void Btn_Thread2_Click(object sender, EventArgs e)
+        {
+            Thread th2 = new Thread(new ThreadStart(Run2));
+            th2.Start();
+        }
+        private void Btn_Thread2_1_Click(object sender, EventArgs e)
+        {
+            Thread th2_1 = new Thread(new ThreadStart(Run2));
+            th2_1.Start();
+        }
+        private void Btn_Thread2_2_Click(object sender, EventArgs e)
+        {
+            Thread th2_2 = new Thread(new ThreadStart(Run2));
+            th2_2.Start();
+        }
+        void Run2()
+        {
+            for(int i=0; i<int.MaxValue; i++)
+            {
+            }
+            Console.WriteLine("Run2 함수 종료");
+        }
+
+        
+    }
+}
